363b435ad207ef6d22779f7d63dedf5c6bd18d06
Rajesh V <rajeshvenu259@gmail.com>
2019-12-12 14:00:53 +0530

    Changed Text

--- app/src/main/java/com/example/ovs/candidate_details.java
+++ app/src/main/java/com/example/ovs/candidate_details.java
@@ -14,7 +14,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+//Added Text
 public class candidate_details extends AppCompatActivity
 {
 
